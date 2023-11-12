042284f0cf2d843c8e5a242a407edb3d0d670013
d10s <79284025+D10S0VSkY-OSS@users.noreply.github.com>
2023-11-12 01:43:55 +0100

    🔧 poc02

--- variables.tf
+++ variables.tf
@@ -5,18 +5,11 @@ variable "region" {
 variable "vpc_cidr" {
   type = string
   description = "The allowed block size is between a /28 netmask and /16 netmask"
-  default = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
   type = map(string)
   description = "Don't change this without first changing vpc_cidr"
-  default = {
-    be1 = "10.0.0.0/24"
-    be2 = "10.0.1.0/24"
-    fe1 = "10.0.2.0/24"
-    fe2 = "10.0.3.0/24"
-  }
 }
 
 variable "vpc_name" {
@@ -31,7 +24,6 @@ variable "vpc_name" {
 variable "simple_string" {
   description = "Una simple variable de cadena"
   type        = string
-  default     = "valorPorDefecto"
 }
 
 # Variable numérica con validación
@@ -49,24 +41,18 @@ variable "number_example" {
 variable "list_example" {
   description = "Ejemplo de variable de lista"
   type        = list(string)
-  default     = ["valor1", "valor2", "valor3"]
 }
 
 # Variable de mapa
 variable "map_example" {
   description = "Ejemplo de variable de mapa"
   type        = map(string)
-  default     = {
-    clave1 = "valor1"
-    clave2 = "valor2"
-  }
 }
 
 # Variable booleana
 variable "boolean_example" {
   description = "Ejemplo de variable booleana"
   type        = bool
-  default     = false
 }
 
 # Variable compleja con validación
